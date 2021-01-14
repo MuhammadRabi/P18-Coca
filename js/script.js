b3a83401200cbabefe28464f599f563f768f0765
Muhammad Rabi <34184092+MuhammadRabi@users.noreply.github.com>
2021-01-14 23:18:11 +0200

    swiper

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ function showNavigation() {
   navigation.classList.toggle("active");
   toggle.classList.toggle("active");
 }
+
+// swiper js
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  loop: true,
+});
